926456831c4d739995d722811af4969d25cfa80a
Soumya121099 <70697901+Soumya121099@users.noreply.github.com>
2020-09-04 16:36:51 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Branches
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int a = 5;
+			int b = 6;
+			if (a + b > 10)
+				Console.WriteLine("The answer is greater than 10.");
 		}
 	}
 }
